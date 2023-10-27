a9a538d08d6e203e8f9ea4d2893dba0ddd354c31
mayankDseu <134508591+mayankDseu@users.noreply.github.com>
2023-10-27 20:31:55 +0530

    Update alarmclk.py

--- alarmclk.py
+++ alarmclk.py
@@ -1,22 +1,22 @@
-from datetime import datetime 
-from playsound import playsound  
+from playsound import playsound
+from datetime import datetime
 
-alarm_time = input("Enter the time of alarm to be set:HH:MM:SS\n")
-alarm_hour=alarm_time[0:2]
-alarm_minute=alarm_time[3:5]
-alarm_seconds=alarm_time[6:8]
-alarm_period = alarm_time[9:11].upper()
-print("Setting up alarm..")
+txt=input("Set the Time of the alarm: HH:MM:SS:AM/PM\n ")
+hour=txt[0:2]
+min=txt[3:5]
+sec=txt[6:8]
+period=txt[9:11].upper()
+print("Setting up alarm.. will be turn on at {}:{}:{}:{}".format(hour,min,sec,period))
 while True:
     now = datetime.now()
     current_hour = now.strftime("%I")
-    current_minute = now.strftime("%M")
-    current_seconds = now.strftime("%S")
+    current_min = now.strftime("%M")
+    current_sec = now.strftime("%S")
     current_period = now.strftime("%p")
-    if(alarm_period==current_period):
-        if(alarm_hour==current_hour):
-            if(alarm_minute==current_minute):
-                if(alarm_seconds==current_seconds):
-                    print("Wake Up!")
+    if(period==current_period):
+        if(hour==current_hour):
+            if(min==current_min):
+                if(sec==current_sec):
+                    print("Get Up!")
                     playsound('audio.mp3')
                     break
